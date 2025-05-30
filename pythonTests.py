25e59e4320929dcc974006f2b510698343ab35bf
Washington Fidelix <fidelix.silva25@gmail.com>
2025-05-30 15:33:02 -0300

    development

--- pythonTests.py
+++ pythonTests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi.testclient import TestClient
 from main import app
 
@@ -13,3 +14,13 @@ def test_hello_world_json():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_list_products():
+    response = client.get("/products")
+    assert response.status_code == 200
+
+
+def test_list_size():
+    response = client.get("/products")
+    assert len(response.json()) == 3
